23023297e0878462e51a3d49e96f2e688a71dddf
Artur <richt3r@mail.ru>
2023-10-21 20:21:41 +0500

    aff trans

--- AffineTransformations/MainWindow.xaml.cs
+++ AffineTransformations/MainWindow.xaml.cs
@@ -21,8 +21,6 @@ namespace AffineTransformations
     /// </summary>
     public partial class MainWindow : Window
     {
-        Regex numeric_regex = new Regex("[^0-9]+");
-        Regex float_regex = new Regex(@"^\-?[0-9]+(?:\.[0-9]+)?$");
         Polygon polygon;
 
         public MainWindow()
@@ -33,33 +31,74 @@ namespace AffineTransformations
             canvas.Children.Add(polygon);
         }
 
-        private void ValidateNumeric(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = numeric_regex.IsMatch(e.Text);
-        }
-
         private void moveButton_Click(object sender, RoutedEventArgs e)
         {
             bool isXValid = Double.TryParse(xTextBox.Text, out double x);
             bool isYValid = Double.TryParse(yTextBox.Text, out double y);
 
-            if (!isXValid && !isYValid) 
+            if (!isXValid || !isYValid) 
             {
-                MessageBox.Show("Некорректные данные.");
+                MessageBox.Show("Некорректные данные.", "Ошибка", MessageBoxButton.OK,MessageBoxImage.Error);
                 return;
             }
 
-            MovePolygon(x, y, canvas, polygon);
+            if (!MovePolygon(x, y, canvas, polygon))
+            {
+                MessageBox.Show("Фигура вышла за пределы экрана!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            };
+
             polygon.InvalidateVisual();
         }
 
-        private bool MovePolygon(double x, double y, Canvas canvas, Polygon polygon)
+        private static bool MovePolygon(double x, double y, Canvas canvas, Polygon polygon)
         {
             PointCollection new_points = new PointCollection();
 
             for (int i = 0; i < polygon.Points.Count; i++)
             {
-                new_points.Add(new Point(polygon.Points[i].X + x, polygon.Points[i].Y + y));
+                new_points.Add(new Point(polygon.Points[i].X + x, polygon.Points[i].Y - y));
+
+                if (new_points.Last().X < 0 || new_points.Last().Y < 0 || new_points.Last().Y > canvas.ActualHeight || new_points.Last().X > canvas.ActualWidth)
+                {
+                    return false;
+                }
+            }
+
+            polygon.Points.Clear();
+            polygon.Points = new_points;
+
+            return true;
+        }
+
+        private static bool RotatePolygon(double angle_radians, Canvas canvas, Polygon polygon)
+        {
+            PointCollection new_points = new PointCollection();
+            double angle_degrees = angle_radians * (Math.PI / 180);
+
+            foreach (Point point in polygon.Points)
+            {
+                new_points.Add(new Point(point.X * Math.Cos(angle_degrees) + point.Y*Math.Sin(angle_degrees), -point.X*Math.Sin(angle_degrees) + point.Y*Math.Cos(angle_degrees)));
+
+                if (new_points.Last().X < 0 || new_points.Last().Y < 0 || new_points.Last().Y > canvas.ActualHeight || new_points.Last().X > canvas.ActualWidth)
+                {
+                    return false;
+                }
+            }
+
+            polygon.Points.Clear();
+            polygon.Points = new_points;
+
+            return true;
+        }
+
+        private static bool ScalePolygon(double x_multiplier, double y_multiplier, Canvas canvas, Polygon polygon)
+        {
+            PointCollection new_points = new PointCollection();
+
+            foreach (Point point in polygon.Points)
+            {
+                new_points.Add(new Point(point.X * x_multiplier, point.Y * y_multiplier));
 
                 if (new_points.Last().X < 0 || new_points.Last().Y < 0 || new_points.Last().Y > canvas.ActualHeight || new_points.Last().X > canvas.ActualWidth)
                 {
@@ -105,12 +144,41 @@ namespace AffineTransformations
 
         private void rotateButton_Click(object sender, RoutedEventArgs e)
         {
+            bool angle_valid = Double.TryParse(rotateTextBox.Text, out double angle_radians);
 
+            if (!angle_valid)
+            {
+                MessageBox.Show("Некорректные данные.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!RotatePolygon(angle_radians, canvas, polygon))
+            {
+                MessageBox.Show("Фигура вышла за пределы экрана!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            };
+
+            polygon.InvalidateVisual();
         }
 
         private void scaleButton_Click(object sender, RoutedEventArgs e)
         {
+            bool isXValid = Double.TryParse(xScaleTextBox.Text, out double x_multiplier);
+            bool isYValid = Double.TryParse(yScaleTextBox.Text, out double y_multiplier);
 
+            if (!isXValid || !isYValid)
+            {
+                MessageBox.Show("Некорректные данные.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!ScalePolygon(x_multiplier, y_multiplier, canvas, polygon))
+            {
+                MessageBox.Show("Фигура вышла за пределы экрана!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            polygon.InvalidateVisual();
         }
     }
 }
