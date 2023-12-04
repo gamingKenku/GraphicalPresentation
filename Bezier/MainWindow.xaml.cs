bd29a03c2af8738a81df9fe886802f864f21014e
Artur <richt3r@mail.ru>
2023-12-04 23:30:24 +0500

    bezier finished not tested

--- Bezier/MainWindow.xaml.cs
+++ Bezier/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Configuration;
+using System.Runtime.Serialization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -14,14 +15,21 @@ namespace Bezier
     {
         private PointCollection anchor_points;
         private Ellipse preview_point;
+        private List<Ellipse> placed_points;
+        private Ellipse? dragged_point;
+
         double t_step = 0.001;
         Path? bezier_path;
+        bool is_dragging;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            is_dragging = false;
+
             anchor_points = new PointCollection();
+            placed_points = new List<Ellipse>();
 
             preview_point = new Ellipse()
             {
@@ -34,6 +42,27 @@ namespace Bezier
             };
 
             Panel.SetZIndex(preview_point, 1);
+        }
+
+        private Path GetBezierPath(PointCollection anchor_points, double t_step)
+        {
+            StreamGeometry bezier = new StreamGeometry();
+
+            using (StreamGeometryContext context = bezier.Open())
+            {
+                context.BeginFigure(anchor_points[0], false, false);
+
+                DrawBezier(anchor_points, context, t_step);
+            }
+
+            Path bezier_path = new Path()
+            {
+                Data = bezier,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1,
+            };
+
+            return bezier_path;
         }
 
         private void DrawBezier(PointCollection anchor_points, StreamGeometryContext context, double t_step)
@@ -106,85 +135,179 @@ namespace Bezier
             };
             Canvas.SetLeft(point_mark, Canvas.GetLeft(preview_point));
             Canvas.SetTop(point_mark, Canvas.GetTop(preview_point));
+            Panel.SetZIndex(point_mark, 1);
 
             canvas.Children.Add(point_mark);
-        }
-
-        private void drawButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (anchor_points.Count < 2)
-            {
-                MessageBox.Show("Количество опорных точек не должно быть меньше двух.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            canvas.Children.Remove(bezier_path);
-
-            StreamGeometry bezier = new StreamGeometry();
-
-            using (StreamGeometryContext context = bezier.Open())
-            {
-                context.BeginFigure(anchor_points[0], false, false);
-
-                DrawBezier(anchor_points, context, t_step);
-            }
-
-            bezier_path = new Path() 
-            {
-                Data = bezier,
-                Stroke = Brushes.Black,
-                StrokeThickness = 1,
-            };
-
-            canvas.Children.Add(bezier_path);
+            placed_points.Add(point_mark);
         }
 
         private void clearButton_Click(object sender, RoutedEventArgs e)
         {
             canvas.Children.Clear();
             anchor_points.Clear();
+            placed_points.Clear();
 
             canvas.Children.Add(preview_point);
+            ClearCanvasEvents();
+            preview_point.Visibility = Visibility.Hidden;
+
+            placePointsToggleButton.IsChecked = false;
+            deletePointsToggleButton.IsChecked = false;
+            movePointsToggleButton.IsChecked = false;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             canvas.Children.Add(preview_point);
-
-            canvas.MouseMove -= canvas_MouseMove;
-            canvas.MouseLeftButtonDown -= canvas_MouseLeftButtonDown;
         }
 
-        private void placePointsToggleButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (placePointstoggleButton.IsChecked == true)
-            {
-                canvas.MouseMove += canvas_MouseMove;
-                canvas.MouseLeftButtonDown += canvas_MouseLeftButtonDown;
-            }
-            else
-            {
-                canvas.MouseMove -= canvas_MouseMove;
-                canvas.MouseLeftButtonDown -= canvas_MouseLeftButtonDown;
-                preview_point.Visibility = Visibility.Hidden;
-            }
-        }
-
-        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        private void canvas_MouseMove_Place(object sender, MouseEventArgs e)
         {
             preview_point.Visibility = canvas.IsMouseOver ? Visibility.Visible : Visibility.Hidden;
 
             UpdatePreviewPoint(e.GetPosition(canvas));
         }
 
-        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void canvas_MouseLeftButtonDown_Place(object sender, MouseButtonEventArgs e)
         {
             AddPoint();
+
+            canvas.Children.Remove(bezier_path);
+            bezier_path = GetBezierPath(anchor_points, t_step);
+            canvas.Children.Add(bezier_path);
+        }
+
+        private void canvas_MouseMove_Delete(object sender, MouseEventArgs e)
+        {
+            Ellipse? point_under_mouse = GetEllipseByPoint(e.GetPosition(canvas));
+
+            foreach (Ellipse point_mark in placed_points)
+            {
+                point_mark.Fill = point_mark == point_under_mouse ? Brushes.Black : Brushes.White;
+            }
+        }
+
+        private void canvas_MouseLeftButtonDown_Delete(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse? point_under_mouse = GetEllipseByPoint(e.GetPosition(canvas));
+
+            if (point_under_mouse == null) return;
+
+            int index = placed_points.IndexOf(point_under_mouse);
+
+            canvas.Children.Remove(point_under_mouse);
+
+            anchor_points.RemoveAt(index);
+            placed_points.RemoveAt(index);
+
+            if (anchor_points.Count == 0) return;
+
+            canvas.Children.Remove(bezier_path);
+            bezier_path = GetBezierPath(anchor_points, t_step);
+            canvas.Children.Add(bezier_path);
+        }
+
+        private void canvas_MouseMove_Move(object sender, MouseEventArgs e)
+        {
+            Ellipse? point_under_mouse = GetEllipseByPoint(e.GetPosition(canvas));
+
+            foreach (Ellipse point_mark in placed_points)
+            {
+                point_mark.Fill = point_mark == point_under_mouse ? Brushes.Black : Brushes.White;
+            }
+            
+            if (is_dragging && dragged_point != null)
+            {
+                Canvas.SetLeft(dragged_point, e.GetPosition(canvas).X - dragged_point.Width / 2);
+                Canvas.SetTop(dragged_point, e.GetPosition(canvas).Y - dragged_point.Height / 2);
+
+                int index = placed_points.IndexOf(dragged_point);
+                anchor_points[index] = new Point(e.GetPosition(canvas).X, e.GetPosition(canvas).Y);
+
+                canvas.Children.Remove(bezier_path);
+                bezier_path = GetBezierPath(anchor_points, t_step);
+                canvas.Children.Add(bezier_path);
+            }
+        }
+
+        private void canvas_MouseLeftButtonDown_Move(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse? point_under_mouse = GetEllipseByPoint(e.GetPosition(canvas));
+
+            if (point_under_mouse == null) return;
+
+            is_dragging = true;
+            dragged_point = point_under_mouse;
+            dragged_point.Fill = Brushes.Black;
+        }
+
+        private void canvas_MouseLeftButtonUp_Move(object sender, MouseButtonEventArgs e)
+        {
+            is_dragging = false;
+            dragged_point = null;
         }
 
         private void border_canvas_MouseMove(object sender, MouseEventArgs e)
         {
             preview_point.Visibility = Visibility.Hidden;
+        }
+
+        private void modeToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearCanvasEvents();
+            preview_point.Visibility = Visibility.Hidden;
+
+            if (placePointsToggleButton.IsChecked == true && sender == placePointsToggleButton)
+            {
+                canvas.MouseMove += canvas_MouseMove_Place;
+                canvas.MouseLeftButtonDown += canvas_MouseLeftButtonDown_Place;
+                deletePointsToggleButton.IsChecked = false;
+                movePointsToggleButton.IsChecked = false;
+            }
+            else if (deletePointsToggleButton.IsChecked == true && sender == deletePointsToggleButton)
+            {
+                canvas.MouseMove += canvas_MouseMove_Delete;
+                canvas.MouseLeftButtonDown += canvas_MouseLeftButtonDown_Delete;
+                placePointsToggleButton.IsChecked = false;
+                movePointsToggleButton.IsChecked = false;
+            }
+            else
+            {
+                canvas.MouseMove += canvas_MouseMove_Move;
+                canvas.MouseLeftButtonDown += canvas_MouseLeftButtonDown_Move;
+                canvas.MouseLeftButtonUp += canvas_MouseLeftButtonUp_Move;
+                deletePointsToggleButton.IsChecked = false;
+                placePointsToggleButton.IsChecked = false;
+            }
+        }
+
+        private void ClearCanvasEvents()
+        {
+            canvas.MouseMove -= canvas_MouseMove_Place;
+            canvas.MouseLeftButtonDown -= canvas_MouseLeftButtonDown_Place;
+            canvas.MouseMove -= canvas_MouseMove_Delete;
+            canvas.MouseLeftButtonDown -= canvas_MouseLeftButtonDown_Delete;
+            canvas.MouseMove -= canvas_MouseMove_Move;
+            canvas.MouseLeftButtonDown -= canvas_MouseLeftButtonDown_Move;
+            canvas.MouseLeftButtonUp -= canvas_MouseLeftButtonUp_Move;
+        }
+
+        private Ellipse? GetEllipseByPoint(Point point)
+        {
+            Ellipse? res = null;
+            EllipseGeometry temp_geometry;
+
+            foreach (Ellipse point_mark in placed_points)
+            {
+                temp_geometry = new EllipseGeometry(new Rect(Canvas.GetLeft(point_mark), Canvas.GetTop(point_mark), point_mark.Width, point_mark.Height));
+
+                if (temp_geometry.FillContains(point))
+                {
+                    res = point_mark;
+                }
+            }
+
+            return res;
         }
     }
 }
